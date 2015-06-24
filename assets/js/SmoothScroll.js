42864dac166de4410d94c73071cd6a36ad3a5cf4
flipmotion <flipmotions@gmail.com>
2015-06-24 17:31:05 +0300

    all done
    all test passed all`s ok

--- assets/js/SmoothScroll.js
+++ assets/js/SmoothScroll.js
@@ -11,9 +11,9 @@
 var defaultOptions = {
 
     // Scrolling Core
-    frameRate        : 150, // [Hz]
+    frameRate        : 300, // [Hz]
     animationTime    : 400, // [px]
-    stepSize         : 120, // [px]
+    stepSize         : 50, // [px]
 
     // Pulse (less tweakable)
     // ratio of "tail" to "acceleration"
